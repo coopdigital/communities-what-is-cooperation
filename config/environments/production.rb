68134cd7874eabc4f1c14dcf96898beeb22cc208
James Almond <james@jamesalmond.com>
2017-12-06 15:45:54 +0000

    Password protect admin page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  if ENV["ADMIN_PASSWORD"].blank?
+    raise 'ADMIN_PASSWORD NOT SET'
+  end
 end
