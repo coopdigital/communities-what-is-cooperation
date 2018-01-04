2da555db2053a7bfc2338e79bd2dc0cb4f879cb4
James Almond <james@jamesalmond.com>
2018-01-04 15:43:48 +0000

    Collected involvement responses and activity stats

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -5,6 +5,10 @@ module Admin
 
     def index
       @members = Member.order('created_at ASC')
+      @involvement_responses = Submission.where.not(involvement: "").pluck(:involvement).uniq
+      @activity_suggestions = Submission.where.not(activity_suggestion: "").pluck(:activity_suggestion).uniq
+      @interests = Activity.with_thumbs_up.map{|activity| [activity, activity.interests.thumbs_up.count]}.sort_by(&:last).reverse
+      @disinterests = Activity.with_thumbs_down.map{|activity| [activity, activity.interests.thumbs_down.count]}.sort_by(&:last).reverse
     end
 
     def create
