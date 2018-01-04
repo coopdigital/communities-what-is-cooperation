62c9c2a30b98d7b043a91f1a3b1b24169cd26f2d
James Almond <james@jamesalmond.com>
2018-01-04 16:41:52 +0000

    Ensure foreign key is delete

--- db/migrate/20180104162302_add_foreign_keys.rb
+++ db/migrate/20180104162302_add_foreign_keys.rb
@@ -1,6 +1,8 @@
 class AddForeignKeys < ActiveRecord::Migration[5.1]
   def change
     add_foreign_key :submissions, :members, on_delete: :cascade
+    remove_foreign_key :interests, :submissions
+    remove_foreign_key :interests, :activities
     add_foreign_key :interests, :submissions, on_delete: :cascade
     add_foreign_key :interests, :activities, on_delete: :cascade
   end
