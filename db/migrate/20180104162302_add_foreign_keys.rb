7e9ac8024c251013d3b771f40a12e3c8a9bab540
James Almond <james@jamesalmond.com>
2018-01-04 16:28:58 +0000

    Foreign key constraints on db

---
+++ db/migrate/20180104162302_add_foreign_keys.rb
@@ -0,0 +1,7 @@
+class AddForeignKeys < ActiveRecord::Migration[5.1]
+  def change
+    add_foreign_key :submisions, :members, on_delete: :cascade
+    add_foreign_key :interests, :submissions, on_delete: :cascade
+    add_foreign_key :interests, :activities, on_delete: :cascade
+  end
+end
