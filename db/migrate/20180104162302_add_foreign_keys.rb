077c06a750f3a08bc24c3076f1bc179da281d4ba
James Almond <james@jamesalmond.com>
2018-01-04 16:38:42 +0000

    Fix submissions typo

--- db/migrate/20180104162302_add_foreign_keys.rb
+++ db/migrate/20180104162302_add_foreign_keys.rb
@@ -1,6 +1,6 @@
 class AddForeignKeys < ActiveRecord::Migration[5.1]
   def change
-    add_foreign_key :submisions, :members, on_delete: :cascade
+    add_foreign_key :submissions, :members, on_delete: :cascade
     add_foreign_key :interests, :submissions, on_delete: :cascade
     add_foreign_key :interests, :activities, on_delete: :cascade
   end
